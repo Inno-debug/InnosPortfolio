17ce7ceede9725159499777af3bc0c9e2a279afe
Innur-SYS <inno.antoskiewicz@gmail.com>
2025-07-11 14:09:57 -0400

    logos completed

--- InnosPortfolio/obj/Debug/net9.0/InnosPortfolio.AssemblyInfo.cs
+++ InnosPortfolio/obj/Debug/net9.0/InnosPortfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InnosPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbc01994e2820c6049bc8817e5475e723318bdbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+936f01291c860132744fece600f243ac5b6c91c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("InnosPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InnosPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
